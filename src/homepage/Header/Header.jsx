a0ee063afce52c5944816656fbf6a52a28fa28f2
DevBigEazi <adesholatajudeen1@gmail.com>
2023-04-12 12:47:36 +0100

    Navbar-and-Head-Section update #1

--- src/homepage/Header/Header.jsx
+++ src/homepage/Header/Header.jsx
@@ -1,12 +1,24 @@
-import './Header.scss';
-import { images } from '../../images';
+import "./Header.scss";
+import { images } from "../../images";
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="header container">
+      <div className="containerLeft">
+        <h1>
+          Corem ipsum dolor sit amet, <span>consectetur adipiscing elit.</span>
+        </h1>
+        <div className="btn genBtn">
+          <Link to="/join">Apply Now</Link>
+        </div>
+      </div>
+      <div className="containerRight">
+        <img src={images.hero} alt="Hero image" />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
