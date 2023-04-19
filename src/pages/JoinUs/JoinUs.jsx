2659d89805cb70ed5c5de10e7d1e4aa1a3fdc1ab
DevBigEazi <adesholatajudeen1@gmail.com>
2023-04-19 11:36:14 +0100

    Registration from to post sanity

--- src/pages/JoinUs/JoinUs.jsx
+++ src/pages/JoinUs/JoinUs.jsx
@@ -2,10 +2,28 @@ import "./JoinUs.scss";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
 
 import { client } from "../../client";
 
 const JoinUs = () => {
+  const [success, setSuccess] = useState(false);
+  //   const [formData, setFormData] = useState({
+  //     firstname: "",
+  //     lastname: "",
+  //     stateOfOrigin: "",
+  //     gender: "",
+  //     dob: "",
+  //     phoneNumber: "",
+  //     AcadQual: "",
+  //     email: "",
+  //     exp: "",
+  //     html: "",
+  //     css: "",
+  //     javascript: "",
+  //     other: "",
+  //   });
+
   const schema = yup.object().shape({
     firstname: yup
       .string()
@@ -63,7 +81,7 @@ const JoinUs = () => {
     html: yup.string(),
     css: yup.string(),
     javascript: yup.string(),
-    javascript: yup.string(),
+    other: yup.string(),
   });
 
   const {
@@ -74,172 +92,230 @@ const JoinUs = () => {
     resolver: yupResolver(schema),
   });
 
-  const onSubmit = (data) => {
+  //   const {
+  //     firstname,
+  //     lastname,
+  //     stateOfOrigin,
+  //     gender,
+  //     dob,
+  //     phoneNumber,
+  //     AcadQual,
+  //     email,
+  //     exp,
+  //     html,
+  //     css,
+  //     javascript,
+  //     other,
+  //   } = formData;
+
+  const onSubmit = async (data) => {
     console.log(data);
-    // client.create;
+
+    const join = {
+      _type: "join",
+      firstname: data.firstname,
+      lastname: data.lastname,
+      stateOfOrigin: data.stateOfOrigin,
+      gender: data.gender,
+      dob: data.dob,
+      phoneNumber: data.phoneNumber,
+      AcadQual: data.AcadQual,
+      email: data.email,
+      exp: data.exp,
+      html: data.html,
+      css: data.css,
+      javascript: data.javascript,
+      other: data.other,
+    };
+
+    const result = await client
+      .create(join)
+      .then(() => {
+        setSuccess(true);
+      })
+      .catch((err) => console.log(err));
+
+    return result;
   };
 
   return (
     <section className="joinUs container">
-      <div className="headerText">
-        <h2>Join Next Cohort</h2>
-        <small className="pText">Fill the form below:</small>
-      </div>
-
-      <div className="formContainer">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <article>
-            <div>
-              <label htmlFor="firstname">Firstname</label>
-              <input type="text" id="firstname" {...register("firstname")} />
-              <p
-                className={`${errors.firstname ? "instruction" : "offscreen"}`}
-              >
-                {errors.firstname?.message}
-              </p>
-            </div>
-            <div>
-              <label htmlFor="lastname">Lastname</label>
-              <input type="text" id="lastname" {...register("lastname")} />
-              <p className={`${errors.lastname ? "instruction" : "offscreen"}`}>
-                {errors.lastname?.message}
-              </p>
-            </div>
-            <div>
-              <label htmlFor="stateOfOrigin">State Of Origin</label>
-              <input
-                type="text"
-                id="stateOfOrigin"
-                {...register("stateOfOrigin")}
-              />
-              <p
-                className={`${
-                  errors.stateOfOrigin ? "instruction" : "offscreen"
-                }`}
-              >
-                {errors.stateOfOrigin?.message}
-              </p>
-            </div>
-            <div>
-              <label htmlFor="gender">Gender</label>
-              <select name="gender" id="gender" {...register("gender")}>
-                <option value="">select an option</option>
-
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
-              <p className={`${errors.gender ? "instruction" : "offscreen"}`}>
-                {errors.gender?.message}
-              </p>
-            </div>
-            <div>
-              <label htmlFor="dob">Date of birth</label>
-              <input
-                type="date"
-                id="dob"
-                placeholder="DD/MM/YY"
-                {...register("dob")}
-              />
-              <p className={`${errors.dob ? "instruction" : "offscreen"}`}>
-                {errors.dob?.message}
-              </p>
-            </div>
-            <div>
-              <label htmlFor="phoneNumber">Phone Number</label>
-              <input
-                type="number"
-                id="phoneNumber"
-                placeholder="+2348100000000"
-                {...register("phoneNumber")}
-              />
-              <p
-                className={`${
-                  errors.phoneNumber ? "instruction" : "offscreen"
-                }`}
-              >
-                {errors.phoneNumber?.message}
-              </p>
-            </div>
-            <div>
-              <label htmlFor="AcadQual">Academy Qualification</label>
-              <select name="AcadQual" id="AcadQual" {...register("AcadQual")}>
-                <option value="">select an option</option>
-
-                <option value="PhD/Master Degree">PhD/Master Degree</option>
-                <option value="Bsc/Ba/HND">Bsc/Ba/HND</option>
-                <option value="ND">ND</option>
-                <option value="Secondary School Certificate">
-                  Secondary School Certificate
-                </option>
-                <option value="Others">Others</option>
-              </select>
-              <p className={`${errors.AcadQual ? "instruction" : "offscreen"}`}>
-                {errors.AcadQual?.message}
-              </p>
-            </div>
-            <div>
-              <label htmlFor="email">E-mail</label>
-              <input type="email" id="email" {...register("email")} />
-              <p className={`${errors.email ? "instruction" : "offscreen"}`}>
-                {errors.email?.message}
-              </p>
-            </div>
-
-            <div>
-              <label htmlFor="exp">Coding Experience</label>
-              <select name="exp" id="exp" {...register("exp")}>
-                <option value="">select an option</option>
-
-                <option value="Advanced">Advanced</option>
-                <option value="Mid-Level">Mid-Level</option>
-                <option value="Beginner">Beginner</option>
-                <option value="No experience">No experience</option>
-              </select>
-              <p className={`${errors.exp ? "instruction" : "offscreen"}`}>
-                {errors.exp?.message}
-              </p>
-            </div>
-          </article>
-
-          <aside>
-            <p>Do You Know:</p>
-            <input
-              type="checkbox"
-              id="html"
-              value="HTML"
-              {...register("html")}
-            />
-            <label htmlFor="html">HTML</label>
-            <br />
-
-            <input type="checkbox" id="css" value="CSS" {...register("css")} />
-            <label htmlFor="css">CSS</label>
-            <br />
-
-            <input
-              type="checkbox"
-              id="javascript"
-              value="Javascript"
-              {...register("javascript")}
-            />
-            <label htmlFor="javascript">Javascript</label>
-            <br />
-
-            <input
-              type="checkbox"
-              id="other"
-              value="None of the above"
-              {...register("other")}
-            />
-            <label htmlFor="other">None of the above</label>
-            <br />
-          </aside>
-
-          <div className="btnCon">
-            <button className="genBtn">Submit</button>
+      {!success ? (
+        <>
+          <div className="headerText">
+            <h2>Join Next Cohort</h2>
+            <small className="pText">Fill the form below:</small>
           </div>
-        </form>
-      </div>
+
+          <div className="formContainer">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <article>
+                <div>
+                  <label htmlFor="firstname">Firstname</label>
+                  <input
+                    type="text"
+                    id="firstname"
+                    {...register("firstname")}
+                  />
+                  <p
+                    className={`${
+                      errors.firstname ? "instruction" : "offscreen"
+                    }`}
+                  >
+                    {errors.firstname?.message}
+                  </p>
+                </div>
+                <div>
+                  <label htmlFor="lastname">Lastname</label>
+                  <input type="text" id="lastname" {...register("lastname")} />
+                  <p
+                    className={`${
+                      errors.lastname ? "instruction" : "offscreen"
+                    }`}
+                  >
+                    {errors.lastname?.message}
+                  </p>
+                </div>
+                <div>
+                  <label htmlFor="stateOfOrigin">State Of Origin</label>
+                  <input
+                    type="text"
+                    id="stateOfOrigin"
+                    {...register("stateOfOrigin")}
+                  />
+                  <p
+                    className={`${
+                      errors.stateOfOrigin ? "instruction" : "offscreen"
+                    }`}
+                  >
+                    {errors.stateOfOrigin?.message}
+                  </p>
+                </div>
+                <div>
+                  <label htmlFor="gender">Gender</label>
+                  <select name="gender" id="gender" {...register("gender")}>
+                    <option value="">select an option</option>
+
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                  <p
+                    className={`${errors.gender ? "instruction" : "offscreen"}`}
+                  >
+                    {errors.gender?.message}
+                  </p>
+                </div>
+                <div>
+                  <label htmlFor="dob">Date of birth</label>
+                  <input
+                    type="date"
+                    id="dob"
+                    placeholder="DD/MM/YY"
+                    {...register("dob")}
+                  />
+                  <p className={`${errors.dob ? "instruction" : "offscreen"}`}>
+                    {errors.dob?.message}
+                  </p>
+                </div>
+                <div>
+                  <label htmlFor="phoneNumber">Phone Number</label>
+                  <input
+                    type="number"
+                    id="phoneNumber"
+                    placeholder="+2348100000000"
+                    {...register("phoneNumber")}
+                  />
+                  <p
+                    className={`${
+                      errors.phoneNumber ? "instruction" : "offscreen"
+                    }`}
+                  >
+                    {errors.phoneNumber?.message}
+                  </p>
+                </div>
+                <div>
+                  <label htmlFor="AcadQual">Academy Qualification</label>
+                  <select
+                    name="AcadQual"
+                    id="AcadQual"
+                    {...register("AcadQual")}
+                  >
+                    <option value="">select an option</option>
+
+                    <option value="PhD/Master Degree">PhD/Master Degree</option>
+                    <option value="Bsc/Ba/HND">Bsc/Ba/HND</option>
+                    <option value="ND">ND</option>
+                    <option value="Secondary School Certificate">
+                      Secondary School Certificate
+                    </option>
+                    <option value="Others">Others</option>
+                  </select>
+                  <p
+                    className={`${
+                      errors.AcadQual ? "instruction" : "offscreen"
+                    }`}
+                  >
+                    {errors.AcadQual?.message}
+                  </p>
+                </div>
+                <div>
+                  <label htmlFor="email">E-mail</label>
+                  <input type="email" id="email" {...register("email")} />
+                  <p
+                    className={`${errors.email ? "instruction" : "offscreen"}`}
+                  >
+                    {errors.email?.message}
+                  </p>
+                </div>
+
+                <div>
+                  <label htmlFor="exp">Coding Experience</label>
+                  <select name="exp" id="exp" {...register("exp")}>
+                    <option value="">select an option</option>
+
+                    <option value="Advanced">Advanced</option>
+                    <option value="Mid-Level">Mid-Level</option>
+                    <option value="Beginner">Beginner</option>
+                    <option value="No experience">No experience</option>
+                  </select>
+                  <p className={`${errors.exp ? "instruction" : "offscreen"}`}>
+                    {errors.exp?.message}
+                  </p>
+                </div>
+              </article>
+
+              <aside>
+                <p>Do You Know:</p>
+                <input type="checkbox" id="html" {...register("html")} />
+                <label htmlFor="html">HTML</label>
+                <br />
+
+                <input type="checkbox" id="css" {...register("css")} />
+                <label htmlFor="css">CSS</label>
+                <br />
+
+                <input
+                  type="checkbox"
+                  id="javascript"
+                  {...register("javascript")}
+                />
+                <label htmlFor="javascript">Javascript</label>
+                <br />
+
+                <input type="checkbox" id="other" {...register("other")} />
+                <label htmlFor="other">None of the above</label>
+                <br />
+              </aside>
+
+              <div className="btnCon">
+                <button className="genBtn">Submit</button>
+              </div>
+            </form>
+          </div>
+        </>
+      ) : (
+        <div>Thank you</div>
+      )}
     </section>
   );
 };
