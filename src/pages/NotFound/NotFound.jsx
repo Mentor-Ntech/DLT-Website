e793b305133d3f0d7cca44e82e111637220225fa
DevBigEazi <adesholatajudeen1@gmail.com>
2023-04-23 18:56:29 +0100

    Error page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import "./NotFound.scss";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className="not-found">
+      <div className="container not-found-container">
+        <h2>404</h2>
+        <h5>Oops! Page Not Found</h5>
+        <p>The page you are trying to access doesn't exit</p>
+
+        <div className="sm-div2">
+          <h2>
+            <a href="/">Back to homepage</a>
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
