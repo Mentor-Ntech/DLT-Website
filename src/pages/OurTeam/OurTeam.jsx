7b33f4c7c72093922cb1aaca304bbaeecc75f161
DevBigEazi <adesholatajudeen1@gmail.com>
2023-04-23 15:13:33 +0100

    Our team page updated

--- src/pages/OurTeam/OurTeam.jsx
+++ src/pages/OurTeam/OurTeam.jsx
@@ -1,52 +1,43 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-// import { images } from 'assets';
-
-import './OurTeam.scss';
-import { client, urlFor} from '../../client';
-import images from '../../images/images';
-
-// const team = [
-//   {name: 'Aliu Musa', position: 'Director', desceiption: 'i am a good developer', imgUrl: images},
-// ]
+import "./OurTeam.scss";
+import { client, urlFor } from "../../client";
+import { FaTwitter, FaLinkedinIn } from "react-icons/fa";
 
 const OurTeam = () => {
-  const [team, setTeam] = useState([])
+  const [team, setTeam] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "team"]';
-    client.fetch(query)
-    .then((data) => setTeam(data))
-  }, [])
+    client.fetch(query).then((data) => setTeam(data));
+  }, []);
 
   return (
-    <section className='ourTeam'>
+    <section className="ourTeam">
       <h2>OUR TEAM</h2>
-      <p className='pText'>Meet the DLT team</p>
-      <div className='profileCard directorCard'>
-          <img src={images.Director}/>
-          <h3>Aliu Musa</h3>
-          <h3 className='position'>Director</h3>
-          <p className='pText'>
-            "Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit. Nunc vulputate libero et velit interdum, 
-            ac aliquet odio mattis. Class aptent taciti 
-            sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-          </p>
-        </div>
-      <div className='profileContainer'>
+      <p className="pText">Meet the DLT team</p>
+
+      <div className="profileContainer">
         {team.map((team, index) => (
-          <div key={index} className='profileCard'>
+          <div key={index} className="profileCard">
             <img src={urlFor(team.imgUrl)} alt={team.title} />
             <h3>{team.name}</h3>
-            <h3 className='position'>{team.position}</h3>
-            <p className='pText'>"{team.description}"</p>
+            <h3 className="position">{team.position}</h3>
+            <p className="pText">"{team.description}"</p>
+            <div className="socialIcon">
+              <a href={team.linkedinLink} target="_blank">
+                <FaLinkedinIn />
+              </a>
+
+              <a href={team.twitterLink} target="_blank">
+                <FaTwitter />
+              </a>
+            </div>
           </div>
         ))}
       </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default OurTeam
+export default OurTeam;
