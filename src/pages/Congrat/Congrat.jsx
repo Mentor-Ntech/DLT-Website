3c0a4a7cd0552c570caba70b606723da4e13ff39
DevBigEazi <adesholatajudeen1@gmail.com>
2023-09-27 16:37:43 +0100

    added a pay later button and included account details menu

--- src/pages/Congrat/Congrat.jsx
+++ src/pages/Congrat/Congrat.jsx
@@ -1,26 +1,30 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { GrClose } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 import "./Congrat.scss";
 
 const paymentData = [
   {
     title: "Full payment",
-    price: "#570,000",
-    type: "(Make full payment)",
+    fullstack: { name: "Full Stack ", price: "- #570,000" },
+    frontend: { name: "FrontEnd ", price: "- #370,000" },
+    productDesign: { name: "Product Design ", price: "- #150,000" },
     button: "Pay Now In Full",
   },
   {
     title: "Pay 40% upfront",
-    price: "#228,000",
-    type: "",
-    button: "Pay Now In Full",
+    fullstack: { name: "Full Stack ", price: "- #228,000" },
+    frontend: { name: "FrontEnd ", price: "- #160,000" },
+    productDesign: { name: "Product Design ", price: "- #80,000" },
+    button: "Pay 40% now",
   },
 ];
 
 const Congrat = () => {
-  const [toggle, setToggle] = useState(false);
+  const [pay, setPay] = useState(false);
+  const [payLater, setPayLater] = useState(false);
 
   return (
     <section className="sectionCongrat">
@@ -28,16 +32,32 @@ const Congrat = () => {
         <div className="congratContainer">
           <h2>CONGRATULATIONS!!!</h2>
           <p>Your application has been successfully submitted.</p>
-          <button onClick={() => setToggle(true)}>Proceed to Payment</button>
+          <button
+            onClick={() => {
+              setPay(true);
+              setPayLater(false);
+            }}
+          >
+            Proceed to Payment
+          </button>
+          <button
+            className="payLaterBtn"
+            onClick={() => {
+              setPayLater(true);
+              setPay(false);
+            }}
+          >
+            Pay Later
+          </button>
         </div>
-        {toggle && (
+        {pay && (
           <motion.div
             whileInView={{ x: [260, 0] }}
             transition={{ duration: 0.85, ease: "easeOut" }}
             className="paymentsOptions"
           >
             <div className="closeIcon">
-              <GrClose onClick={() => setToggle(false)} />
+              <GrClose onClick={() => setPay(false)} />
             </div>
             <div className="paymentsOptionsDetails">
               <div className="titles">
@@ -51,12 +71,48 @@ const Congrat = () => {
                 {paymentData.map((item, i) => (
                   <div key={i}>
                     <h4>{item.title}</h4>
-                    <p>{item.price}</p>
-                    <small>{item.type}</small>
+                    <p>
+                      {item.fullstack.name}
+                      <span>{item.fullstack.price}</span>
+                    </p>
+                    <p>
+                      {item.frontend.name}
+                      <span>{item.frontend.price}</span>
+                    </p>
+                    <p>
+                      {item.productDesign.name}
+                      <span>{item.productDesign.price}</span>
+                    </p>
                     <button>{item.button}</button>
                   </div>
                 ))}
               </div>
+            </div>
+          </motion.div>
+        )}
+        {payLater && (
+          <motion.div
+            whileInView={{ x: [260, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+            className="paymentsOptions accountDetails"
+          >
+            <div className="closeIcon">
+              <GrClose onClick={() => setPayLater(false)} />
+            </div>
+            <div className="account">
+              <h2>DLT Africa Account Details</h2>
+              <p>Account Name: DLT Africa Space Limited</p>
+              <p>Account Number: 1709346763</p>
+              <p>Bank: Access Bank </p>
+
+              <small>
+                Send Proof of payment to this number via Whatsapp:&nbsp;
+                <span>
+                  <Link to="https://wa.me/2348156509701" target="_blank">
+                    +234 815 (650) 9701
+                  </Link>
+                </span>
+              </small>
             </div>
           </motion.div>
         )}
